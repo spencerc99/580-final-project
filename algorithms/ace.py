a2a5db7a89b607ac7f3fc7fe946328c6972ba5d2
Will Koh <kyungchan.koh@gmail.com>
2019-04-23 16:19:52 -0500

    Initial commit of algorithms. ACE is implemented. Requires a working hash function.

--- algorithms/ace.py
+++ algorithms/ace.py
@@ -7,35 +7,49 @@
 #
 # The data structure is described in: https://arxiv.org/pdf/1706.06664.pdf
 
-import similarity
+from array import array
+import pandas as pd
+from .similarity import HashFunc
 
 class ACE():
   # Class representation of ACE.
 
-  def __init__(self, K, L):
+  def __init__(self, K, L, alpha):
     self.K = K
     self.L = L
-    self.arrays = [[0 for i in range(2 ** K)] for i in range(L)]
+    self.alpha = alpha
+    self.arrays = [array('h') for j in range(self.L)]  # 'h' for signed shorts. 'H' for unsigned shorts.
+    for j in range(self.L):
+      for _ in range(2 ** K):
+        self.arrays[j].append(0)
+    self.mu = 0
+    self.n = 0
 
   def preprocess(self, D):
     '''
     Preprocesses a dataset into ACE.
 
-    @param D  The dataset.
+    @param D  The dataset represented using a pandas dataframe.
     '''
-    pass
+    for idx, row in D.iterrows():
+      mu_incre = 0
+      for j in range(self.L):
+        hash_val = HashFunc.some_hash_func(row, j)
+        self.arrays[j][hash_val] += 1
+        mu_incre += ((2 * self.arrays[j][hash_val] + 1) / (self.L * 1.0))
+      self.mu = ((self.n * self.mu) + mu_incre) / ((self.n + 1) * 1.0)
+      self.n += 1
 
   def query(self, q):
     '''
     Queries ACE for anomaly detection.
 
-    @param q  The query.
+    @param q  The query item represented as a row in the dataframe.
+    @returns  True if q is an anomaly. False otherwise.
     '''
-    pass
+    score = 0
+    for j in range(self.L):
+      hash_val = HashFunc.some_hash_func(q, j)
+      score += ((self.arrays[j][hash_val]) / (self.L * 1.0))
 
-  def insert(self, x):
-    '''
-    Inserts a new element into ACE.
-
-    @param x  The new element.
-    '''
+    return (score <= (self.mu - self.alpha))
--- algorithms/similarity.py
+++ algorithms/similarity.py
@@ -7,18 +7,29 @@
 
 import math
 import numpy as np
+import pandas as pd
 
-def SRP(x, y):
-  '''
-  Given two vectors, computes the SRP using cosine similarity.
+class HashFunc():
+  def SRP(x, y):
+    '''
+    Given two vectors, computes the SRP using cosine similarity.
 
-  The equation is (in latex): $1 - \\frac{\\theta}{\\pi}$, where
-  $\\theta = cos^{-1}\\left(\\frac{x^Ty}{||x||_2||y||_2}\\right)$
+    The equation is (in latex): $1 - \\frac{\\theta}{\\pi}$, where
+    $\\theta = cos^{-1}\\left(\\frac{x^Ty}{||x||_2||y||_2}\\right)$
 
-    @param x  First vector of numbers.
-    @param y  Second vector of numbers.
-    @returns  The SRP of x and y.
-  '''
-  numerator = np.dot(x, y)
-  denominator = np.linalg.norm(x) * np.linalg.norm(y)
-  return 1 - math.acos(numerator / denominator) / math.pi
+      @param x  First vector of numbers.
+      @param y  Second vector of numbers.
+      @returns  The SRP of x and y.
+    '''
+    numerator = np.dot(x, y)
+    denominator = np.linalg.norm(x) * np.linalg.norm(y)
+    return 1 - math.acos(numerator / denominator) / math.pi
+
+  def some_hash_func(key, j):
+    '''
+    Temporary placeholder for a hash function.
+
+    @param key  A row in a pandas dataframe.
+    @param j    The jth hash function to be used.
+    '''
+    pass
