83e1fa70b2106b71ecbe9bcb0c42c5332a4eedb2
Spencer Chang <spencerc99@gmail.com>
2019-04-26 13:43:57 -0500

    fix ace.py syntax

--- algorithms/ace.py
+++ algorithms/ace.py
@@ -3,7 +3,7 @@
 # Final Project
 #
 # This file contains an implementation of Array of (Locally Sensitive)
-# Count Estimations (ACE). 
+# Count Estimations (ACE).
 #
 # The data structure is described in: https://arxiv.org/pdf/1706.06664.pdf
 
@@ -12,46 +12,49 @@ import pandas as pd
 from .similarity import HashFunc
 from .vectorize import Sentence2Vec
 
+
 class ACE():
-  # Class representation of ACE.
+    # Class representation of ACE.
 
-  def __init__(self, K, L, alpha):
-    self.K = K
-    self.L = L
-    self.alpha = alpha
-    self.arrays = [array('h') for j in range(self.L)]  # 'h' for signed shorts. 'H' for unsigned shorts.
-    for j in range(self.L):
-      for _ in range(2 ** K):
-        self.arrays[j].append(0)
-    self.mu = 0
-    self.n = 0
-    self.hash_funcs = HashFunc(Sentence2Vec())
+    def __init__(self, K, L, alpha):
+        self.K = K
+        self.L = L
+        self.alpha = alpha
+        # 'h' for signed shorts. 'H' for unsigned shorts.
+        self.arrays = [array('h') for j in range(self.L)]
+        for j in range(self.L):
+            for _ in range(2 ** K):
+                self.arrays[j].append(0)
+        self.mu = 0
+        self.n = 0
+        self.hash_funcs = HashFunc(Sentence2Vec())
 
-  def preprocess(self, D):
-    '''
-    Preprocesses a dataset into ACE.
+    def preprocess(self, D):
+        '''
+        Preprocesses a dataset into ACE.
 
-    @param D  The dataset represented using a pandas dataframe.
-    '''
-    for idx, row in D.iterrows():
-      mu_incre = 0
-      for j in range(self.L):
-        hash_val = HashFunc.hash_tweet(row, j)
-        self.arrays[j][hash_val] += 1
-        mu_incre += ((2 * self.arrays[j][hash_val] + 1) / (self.L * 1.0))
-      self.mu = ((self.n * self.mu) + mu_incre) / ((self.n + 1) * 1.0)
-      self.n += 1
+        @param D  The dataset represented using a pandas dataframe.
+        '''
+        for idx, row in D.iterrows():
+            mu_incre = 0
+            for j in range(self.L):
+                hash_val = self.hash_funcs.hash_tweet(row, j)
+                self.arrays[j][hash_val] += 1
+                mu_incre += ((2 * self.arrays[j]
+                              [hash_val] + 1) / (self.L * 1.0))
+            self.mu = ((self.n * self.mu) + mu_incre) / ((self.n + 1) * 1.0)
+            self.n += 1
 
-  def query(self, q):
-    '''
-    Queries ACE for anomaly detection.
+    def query(self, q):
+        '''
+        Queries ACE for anomaly detection.
 
-    @param q  The query item represented as a row in the dataframe.
-    @returns  True if q is an anomaly. False otherwise.
-    '''
-    score = 0
-    for j in range(self.L):
-      hash_val = HashFunc.hash_tweet(q, j)
-      score += ((self.arrays[j][hash_val]) / (self.L * 1.0))
+        @param q  The query item represented as a row in the dataframe.
+        @returns  True if q is an anomaly. False otherwise.
+        '''
+        score = 0
+        for j in range(self.L):
+            hash_val = self.hash_funcs.hash_tweet(q, j)
+            score += ((self.arrays[j][hash_val]) / (self.L * 1.0))
 
-    return (score <= (self.mu - self.alpha))
+        return (score <= (self.mu - self.alpha))
